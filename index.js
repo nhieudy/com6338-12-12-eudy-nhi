8bac695a827463f40bc9303a0f6f78f60356b272
Nhi Eudy <nhieudy@Nhis-MacBook-Pro.local>
2025-12-02 22:11:03 -0500

    guessLetter and update screen finished. Need to do previous word

--- index.js
+++ index.js
@@ -1,78 +1,113 @@
 const words = [
-  'bananas',
-  'grapes',
-  'carousel',
-  'milkshake',
-  'javascript',
-  'limousine',
-  'chocolate',
-  'programming',
-  'meatloaf',
-  'ukulele',
-  'mango'
-]
-
-let wins = 0
-let losses = 0
-let currentWord
+  "bananas",
+  "grapes",
+  "carousel",
+  "milkshake",
+  "javascript",
+  "limousine",
+  "chocolate",
+  "programming",
+  "meatloaf",
+  "ukulele",
+  "mango",
+];
 
 class Word {
   constructor(word) {
-    this.word = word
-    this.displayWord = word.replaceAll(/[\w]/g, "_")
-    this.remainingGuesses = 10
-    this.incorrectLetters = []
-    this.correctLetters = []
+    this.word = word;
+    this.displayWord = word.replaceAll(/[\w]/g, "_");
+    this.remainingGuesses = 10;
+    this.incorrectLetters = [];
+    this.correctLetters = [];
   }
 
   // implement the guessLetter function:
-  // guessLetter(letter) {}
+  guessLetter(letter) {
+    console.log(this.word);
+    let unguessedWord = "";
+    let arrayUnguessed = [];
+    this.letter = letter;
+    //If letter is right, add to correctLetters
+    //If letter is wrong, add to incorrectLetters
+    if (this.word.includes(letter)) {
+      //Add to correctLetters
+      this.correctLetters.push(letter);
+      if (this.word.includes(letter)) {
+        for (let i = 0; i < this.word.length; i++) {
+          if (this.correctLetters.includes(this.word[i])) {
+            arrayUnguessed[i] = this.word[i];
+            unguessedWord = arrayUnguessed.join("");
+          } else {
+            arrayUnguessed[i] = "_";
+            unguessedWord = arrayUnguessed.join("");
+          }
+        }
+      }
+      this.displayWord = unguessedWord;
+    } else {
+      //Check if incorrectList array includes guess already
+      console.log(this.incorrectLetters);
+      if (this.incorrectLetters.includes(letter)) {
+        return;
+      } else {
+        this.incorrectLetters.push(letter); //adds letter to incorrect list
+        this.remainingGuesses -= 1; //lower guess counter
+      }
+    }
+  }
 
   // implement the updateScreen function:
-  // updateScreen() {}
+  updateScreen() {
+    let guesses = document.querySelector("#remaining-guesses");
+    guesses.textContent = this.remainingGuesses;
+    let incorrectGuesses = document.querySelector("#incorrect-letters");
+    incorrectGuesses.textContent = this.incorrectLetters;
+    let wordToGuess = document.querySelector("#word-to-guess");
+    wordToGuess.textContent = this.displayWord;
+  }
 
   // implement the isGameOver function:
-  // isGameOver() {}
+  isGameOver() {}
 
   // implement the getWinOrLoss function:
-  // getWinOrLoss() {}
+  getWinOrLoss() {}
 }
 
 function newGame() {
-  const randomWord = words[Math.floor(Math.random() * words.length)]
-  currentWord = new Word(randomWord)
-  currentWord.updateScreen()
+  const randomWord = words[Math.floor(Math.random() * words.length)];
+  currentWord = new Word(randomWord);
+  currentWord.updateScreen();
 }
 
-document.onkeyup = function(e) {
-  const pressedKey = e.key.toLowerCase()
+document.onkeyup = function (e) {
+  const pressedKey = e.key.toLowerCase();
   // early exit for non-letter key presses
-  if (!/^[a-z]{1}$/g.test(pressedKey)) return
+  if (!/^[a-z]{1}$/g.test(pressedKey)) return;
 
   // pass in guessed letter to word obj
-  currentWord.guessLetter(pressedKey)
+  currentWord.guessLetter(pressedKey);
   // allow word obj to update screen
-  currentWord.updateScreen()
+  currentWord.updateScreen();
 
   // check if game is over
-  const gameOver = currentWord.isGameOver()
+  const gameOver = currentWord.isGameOver();
 
   // if game is over, update wins/losses and start new game
   if (gameOver) {
-    const previousWord = document.getElementById('previous-word')
-    const winDisplay = document.getElementById('wins')
-    const lossDisplay = document.getElementById('losses')
-    previousWord.textContent = currentWord.word
-    const result = currentWord.getWinOrLoss()
-    if (result === 'win') {
-      wins++
-      winDisplay.textContent = wins
-    } else if (result === 'loss') {
-      losses++
-      lossDisplay.textContent = losses
+    const previousWord = document.getElementById("previous-word");
+    const winDisplay = document.getElementById("wins");
+    const lossDisplay = document.getElementById("losses");
+    previousWord.textContent = currentWord.word;
+    const result = currentWord.getWinOrLoss();
+    if (result === "win") {
+      wins++;
+      winDisplay.textContent = wins;
+    } else if (result === "loss") {
+      losses++;
+      lossDisplay.textContent = losses;
     }
-    newGame()
+    newGame();
   }
-}
+};
 
-newGame()
+newGame();
